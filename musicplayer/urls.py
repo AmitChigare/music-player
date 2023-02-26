dc9c7156aeddeda255c30750e2992e3e28e97299
AmitChigare <amit.gokakfalls17@gmail.com>
2023-02-26 10:35:03 +0530

    Created project named musicplayer, implemented static, templates and media configuration required for the project

---
+++ musicplayer/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+from . import views
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
