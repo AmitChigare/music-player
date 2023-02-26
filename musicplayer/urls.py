ea9ac178555d87b59b7109cab775a4936e98c2ce
AmitChigare <amit.gokakfalls17@gmail.com>
2023-02-26 15:51:45 +0530

    Implemented Search functionality in home page

--- musicplayer/urls.py
+++ musicplayer/urls.py
@@ -8,5 +8,6 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
+    path("search/", views.search, name="search"),
     path("player/", include("musicapp.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
